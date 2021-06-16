59ae7f80afbb641a7f99c71350a4518f7cb5202d
kitonja <kitonja75@gmail.com>
2021-06-16 23:02:55 +0200

    added relation

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,23 +3,26 @@ import { User } from "../entity/User";
 import { ErrorField } from "../resolvers/Profile";
 import { RequestResponseExpress } from "../types";
 
-ErrorField
+ErrorField;
 
-export const AuthMiddleware: MiddlewareFn<RequestResponseExpress> = async ({ context }, next):Promise<ErrorField> => {
-    if(!context.req.session.userId) {
-        return {
-            field: 'id',
-            message: 'Not authenticated'
-        }
-    }
-    const id = context.req.session.userId;
-    const user = await User.findOne({id});
-    if(!user) {
-        return {
-            field: 'id',
-            message: 'Not authenticated'
-        }
-    }
-    context.req.user = user;
-    return next();
-  };
+export const AuthMiddleware: MiddlewareFn<RequestResponseExpress> = async (
+  { context },
+  next
+): Promise<ErrorField | null> => {
+  if (!context.req.session.userId) {
+    return {
+      field: "id",
+      message: "Not authenticated",
+    };
+  }
+  const id = context.req.session.userId;
+  const user = await User.findOne({ id }, { relations: ["profile"] });
+  if (!user) {
+    return {
+      field: "id",
+      message: "Not authenticated",
+    };
+  }
+  context.req.user = user;
+  return next();
+};
