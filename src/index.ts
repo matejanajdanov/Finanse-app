a9da17a06d6438099044748385697905b8f0f337
mateja.najdanov <mateja.najdanov@asioso.com>
2021-06-21 01:37:53 +0200

    cors fixed

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import 'reflect-metadata';
-import * as express from 'express';
-import { config } from 'dotenv';
-import { createConnection } from 'typeorm';
-import { UserResolver } from './resolvers/User';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
-import * as redis from 'redis';
-import * as session from 'express-session';
-import * as connectRedis from 'connect-redis';
-import { RequestResponseExpress } from './types';
-import { ProfileResolver } from './resolvers/Profile';
+import "reflect-metadata";
+import * as express from "express";
+import { config } from "dotenv";
+import { createConnection } from "typeorm";
+import { UserResolver } from "./resolvers/User";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import * as redis from "redis";
+import * as session from "express-session";
+import * as connectRedis from "connect-redis";
+import * as cors from "cors";
+import { RequestResponseExpress } from "./types";
+import { ProfileResolver } from "./resolvers/Profile";
 
 // Init env file
 config();
@@ -24,11 +25,11 @@ const main = async () => {
   // Create session
   app.use(
     session({
-      name: 'qid',
+      name: "qid",
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
         httpOnly: true,
-        sameSite: 'lax',
+        sameSite: "lax",
       },
       store: new redisStore({ client: redisClient, disableTouch: true }),
       saveUninitialized: false,
@@ -45,7 +46,9 @@ const main = async () => {
     context: ({ req, res }: RequestResponseExpress) => ({ req, res }),
   });
 
-  apolloServer.applyMiddleware({ app });
+  app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+  apolloServer.applyMiddleware({ app, cors: false });
 
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
